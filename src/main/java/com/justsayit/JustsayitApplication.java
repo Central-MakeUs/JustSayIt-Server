bf35bd663505f6e9b59de8db5531e0f6dddce47e
simhani1 <simhani1@gmail.com>
2024-01-07 23:25:08 +0900

    init: 프로젝트를 생성한다

---
+++ src/main/java/com/justsayit/JustsayitApplication.java
@@ -0,0 +1,14 @@
+package com.justsayit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootApplication
+public class JustsayitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustsayitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/justsayit/JustsayitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justsayit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustsayitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
