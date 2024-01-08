63130e86cf3e8517473c5feee2d49847fef76cde
Jonghan Sim <simhani1@gmail.com>
2024-01-09 02:57:00 +0900

    feat: CI/CD 파이프라인을 구축한다 (#2)
    <!-- 제목양식을 지켜주세요! [Feat/#{이슈번호}] {제목~~} -->
    ### Issue
    - #1 
    
    ### Key Point
    - 짧은 기간 내에 빠르게 개발을 하기 위해 elastic beanstalk을 사용한 롤링 배포 선택
    - ci, cd 스크립트 분리
    
    ### Other
    - 없음

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM openjdk:11
+VOLUME /tmp
+ARG JAR_PATH=./build/libs/
+COPY ${JAR_PATH}justsayit-0.0.1-SNAPSHOT.jar ${JAR_PATH}justsayit-0.0.1-SNAPSHOT.jar
+ENTRYPOINT ["java", "-jar", "-Dspring.profiles.active=prod", "build/libs/justsayit-0.0.1-SNAPSHOT.jar"]
---
+++ src/main/java/com/justsayit/beanstalk/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.justsayit.beanstalk;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String checkHealth() {
+        return "healthy";
+    }
+}
