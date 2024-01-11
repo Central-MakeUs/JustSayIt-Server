60c4aa50b2c36f1d3c4bf6f30a0228fedff8ebe3
simhani1 <simhani1@gmail.com>
2024-01-12 02:12:48 +0900

    feat: multipartFile 인터페이스를 매개변수로 추가한다

--- src/main/java/com/justsayit/member/controller/MemberController.java
+++ src/main/java/com/justsayit/member/controller/MemberController.java
@@ -6,10 +6,8 @@ import com.justsayit.member.service.auth.dto.LoginRes;
 import com.justsayit.member.service.auth.usecase.LoginUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequestMapping("/members")
 @RestController
@@ -19,11 +17,11 @@ public class MemberController {
     private final LoginUseCase loginUseCase;
 
     @PostMapping("/login")
-    public ResponseEntity<LoginRes> login(@RequestBody LoginReq req) {
+    public ResponseEntity<LoginRes> login(@RequestBody LoginReq req, @RequestPart MultipartFile multipartFile) {
         LoginRes res = loginUseCase.login(LoginCommand.builder()
                 .loginType(req.getLoginType())
                 .nickname(req.getNickname())
-                .profileImg(req.getProfileImg())
+                .multipartFile(multipartFile)
                 .token(req.getToken())
                 .build());
         return ResponseEntity.ok(res);
--- src/main/java/com/justsayit/member/service/auth/command/LoginCommand.java
+++ src/main/java/com/justsayit/member/service/auth/command/LoginCommand.java
@@ -2,20 +2,21 @@ package com.justsayit.member.service.auth.command;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 public class LoginCommand {
 
     private String token;
-    private String profileImg;
-    private String nickname;
     private String loginType;
+    private String nickname;
+    private MultipartFile multipartFile;
 
     @Builder
-    public LoginCommand(String token, String profileImg, String nickname, String loginType) {
+    public LoginCommand(String token, String loginType, String nickname, MultipartFile multipartFile) {
         this.token = token;
-        this.profileImg = profileImg;
-        this.nickname = nickname;
         this.loginType = loginType;
+        this.nickname = nickname;
+        this.multipartFile = multipartFile;
     }
 }
