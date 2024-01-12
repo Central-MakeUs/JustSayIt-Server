e0824131e0a08608249f668e073fcf5b74589897
simhani1 <simhani1@gmail.com>
2024-01-13 01:56:01 +0900

    fix: json -> form-data로 변경 & dto 필드 값을 수정한다

--- src/main/java/com/justsayit/member/controller/MemberController.java
+++ src/main/java/com/justsayit/member/controller/MemberController.java
@@ -6,7 +6,10 @@ import com.justsayit.member.service.auth.dto.LoginRes;
 import com.justsayit.member.service.auth.usecase.LoginUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RequestMapping("/members")
@@ -18,7 +21,7 @@ public class MemberController {
     private final LoginFacade loginFacade;
 
     @PostMapping("/login")
-    public ResponseEntity<LoginRes> login(@RequestBody LoginReq req, @RequestPart MultipartFile multipartFile) {
+    public ResponseEntity<LoginRes> login(@RequestPart(value = "login-info") LoginReq req, @RequestPart(value = "file", required = false) MultipartFile multipartFile) {
         LoginRes res = loginFacade.login(req, multipartFile);
         return ResponseEntity.ok(res);
     }
