3b7b4b4018ad1d85759d042728c65c5fda6d5e85
simhani1 <simhani1@gmail.com>
2024-01-14 17:37:21 +0900

    feat: 성별, 생일 정보를 추가한다

--- src/main/java/com/justsayit/member/domain/ProfileInfo.java
+++ src/main/java/com/justsayit/member/domain/ProfileInfo.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
+import java.time.LocalDate;
 
 @Getter
 @Embeddable
@@ -15,12 +16,16 @@ public class ProfileInfo {
 
     private String nickname;
     private String profileImg;
+    private Gender gender;
+    private LocalDate birth;
 
     @Builder
-    public ProfileInfo(String nickname, String profileImg) {
+    public ProfileInfo(String nickname, String profileImg, String gender, LocalDate birth) {
         validateLength(nickname);
         this.nickname = nickname;
         this.profileImg = profileImg;
+        this.gender = Gender.valueOf(gender);
+        this.birth = birth;
     }
 
     private void validateLength(String nickname) {
