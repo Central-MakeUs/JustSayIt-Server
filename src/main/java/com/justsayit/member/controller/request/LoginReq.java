8e3eee7aa4969b4f979b86805a05850360e05c0a
simhani1 <simhani1@gmail.com>
2024-01-14 18:03:39 +0900

    feat: 날짜 타입 직렬화 어노테이션을 추가한다

--- src/main/java/com/justsayit/member/controller/request/LoginReq.java
+++ src/main/java/com/justsayit/member/controller/request/LoginReq.java
@@ -1,6 +1,7 @@
 package com.justsayit.member.controller.request;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -12,6 +13,7 @@ public class LoginReq {
 
     private String nickname;
     private String gender;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birth;
     private String loginType;
     private String token;
