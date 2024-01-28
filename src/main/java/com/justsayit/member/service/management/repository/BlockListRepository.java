b321788ef005cf83db88769b115297cec750ab2b
simhani1 <simhani1@gmail.com>
2024-01-28 22:49:04 +0900

    feat: 차단목록 조회 인터페이스를 추가한다

--- src/main/java/com/justsayit/member/service/management/repository/BlockListRepository.java
+++ src/main/java/com/justsayit/member/service/management/repository/BlockListRepository.java
@@ -1,8 +1,15 @@
 package com.justsayit.member.service.management.repository;
 
 import com.justsayit.member.domain.BlockList;
+import com.justsayit.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BlockListRepository extends JpaRepository<BlockList, Long> {
 
+    @Query("select bl.blocked.id from BlockList bl where bl.blocker = :blocker")
+    List<Long> findAllByBlocker(@Param("blocker") Member blocker);
 }
