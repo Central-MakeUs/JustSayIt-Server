bebd1b0031680bedf0d4778cebdf7ff63e1ad4d9
simhani1 <simhani1@gmail.com>
2024-02-04 01:32:50 +0900

    feat: service를 추가한다

---
+++ src/main/java/com/justsayit/mood/service/get/usecase/GetMoodUseCase.java
@@ -0,0 +1,8 @@
+package com.justsayit.mood.service.get.usecase;
+
+import com.justsayit.mood.service.get.dto.GetTodayMoodRes;
+
+public interface GetMoodUseCase {
+
+    GetTodayMoodRes getTodayMoodRecord();
+}
