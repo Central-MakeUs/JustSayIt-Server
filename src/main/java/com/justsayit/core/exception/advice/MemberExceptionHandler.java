af5b577f9b737b6244b9df01698174abdf936240
Jonghan Sim <simhani1@gmail.com>
2024-02-12 20:54:03 +0900

    feat: 애플 로그인 관련 예외 핸들러를 추가한다 (#118)
    <!-- 제목양식을 지켜주세요! [Feat/#{이슈번호}] {제목~~} -->
    ### Issue
    - #117 
    
    ### Key Point
    - 예외 핸들러 추가
    - 예외 메시지 enum 추가
    
    ### Other
    - 없음

--- src/main/java/com/justsayit/core/exception/advice/MemberExceptionHandler.java
+++ src/main/java/com/justsayit/core/exception/advice/MemberExceptionHandler.java
@@ -2,9 +2,7 @@ package com.justsayit.core.exception.advice;
 
 import com.justsayit.core.template.response.BaseResponse;
 import com.justsayit.core.template.response.ResponseCode;
-import com.justsayit.member.exception.AlreadyExistsMemberException;
-import com.justsayit.member.exception.InvalidNicknameLengthException;
-import com.justsayit.member.exception.NoMemberException;
+import com.justsayit.member.exception.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -28,5 +26,23 @@ public class MemberExceptionHandler {
     public ResponseEntity<BaseResponse<Object>> noMemberException(InvalidNicknameLengthException e) {
         return ResponseEntity.badRequest()
                 .body(BaseResponse.ofFail(ResponseCode.INVALID_NICKNAME_LENGTH));
+    }
+
+    @ExceptionHandler(InvalidAppleIdentityToken.class)
+    public ResponseEntity<BaseResponse<Object>> invalidAppleIdentityToken(InvalidAppleIdentityToken e) {
+        return ResponseEntity.badRequest()
+                .body(BaseResponse.ofFail(ResponseCode.INVALID_APPLE_IDENTITY_TOKEN));
+    }
+
+    @ExceptionHandler(FailToVerifyAppleIdentityToken.class)
+    public ResponseEntity<BaseResponse<Object>> failToVerifyAppleIdentityToken(FailToVerifyAppleIdentityToken e) {
+        return ResponseEntity.badRequest()
+                .body(BaseResponse.ofFail(ResponseCode.FAIL_TO_VERIFY_APPLE_IDENTITY_TOKEN));
+    }
+
+    @ExceptionHandler(FailToGetApplePublicKey.class)
+    public ResponseEntity<BaseResponse<Object>> failToGetApplePublicKey(FailToGetApplePublicKey e) {
+        return ResponseEntity.badRequest()
+                .body(BaseResponse.ofFail(ResponseCode.FAIL_TO_GET_APPLE_PUBLIC_KEY));
     }
 }
